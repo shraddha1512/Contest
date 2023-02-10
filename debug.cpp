764ec1cf2f1aa1e11cc80e36395056bb47606dcf
shraddha1512 <56490108+shraddha1512@users.noreply.github.com>
2023-02-11 00:31:27 +0530

    Create debug.cpp

---
+++ debug.cpp
@@ -0,0 +1,19 @@
+#define debug(x) cout<<#x<<" "; _print(x); cout<<endl;
+void _print(long long t) {cout << t;}
+void _print(int t) {cout << t;}
+void _print(string t) {cout << t;}
+void _print(char t) {cout << t;}
+void _print(long double t) {cout << t;}
+void _print(double t) {cout << t;}
+void _print(unsigned long long t) {cout << t;}
+
+template <class T, class V> void _print(pair <T, V> p);
+template <class T> void _print(vector <T> v);
+template <class T> void _print(set <T> v);
+template <class T, class V> void _print(map <T, V> v);
+template <class T> void _print(multiset <T> v);
+template <class T, class V> void _print(pair <T, V> p) {cout << "{"; _print(p.first); cout << ","; _print(p.second); cout << "}";}
+template <class T> void _print(vector <T> v) {cout << "[ "; for (T i : v) {_print(i); cout << " ";} cout << "]";}
+template <class T> void _print(set <T> v) {cout << "[ "; for (T i : v) {_print(i); cout << " ";} cout << "]";}
+template <class T> void _print(multiset <T> v) {cout << "[ "; for (T i : v) {_print(i); cout << " ";} cout << "]";}
+template <class T, class V> void _print(map <T, V> v) {cout << "[ "; for (auto i : v) {_print(i); cout << " ";} cout << "]";}
